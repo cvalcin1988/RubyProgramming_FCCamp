9324026b9667f0f03ab498e57ae53701b4da807b
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-29 15:49:05 -0500

    33:12 Working with strings start

--- giraffe.rb
+++ giraffe.rb
@@ -1,8 +1,6 @@
-character_name = "Cal"
-character_age = "35"
-
-puts ("There once was a man named #{character_name}")
-puts ("he was " + character_age + " years old.")
-character_name = ("Timmy")
-puts ("He really liked the name #{character_name}")
-puts ("but didnt like being " + character_age + ".")
+name = "Mike" #String
+age = 75    #Integer 
+gpa = 3.2   #Float
+ismale = true   #Boolean
+istall = false  #Boolean
+flaws = nil #has no value
