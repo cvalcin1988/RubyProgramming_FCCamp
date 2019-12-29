2b67da1b02f2808d0fb6adc11c5c3258eccf1a93
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-29 16:21:01 -0500

    44:03, Strings Finish,  Math and Numbers start

--- giraffe.rb
+++ giraffe.rb
@@ -1,6 +1,8 @@
-name = "Mike" #String
-age = 75    #Integer 
-gpa = 3.2   #Float
-ismale = true   #Boolean
-istall = false  #Boolean
-flaws = nil #has no value
+phrase = "Giraffe Academy"
+puts phrase[0,3] 
+puts phrase.length #prints the number of characters in a string
+# prints a range of strings starts from position 0, ('Gir' is the output)
+# doesnt include the last index (character 'a')
+puts phrase.index("ffe") #useful method,tells what position 
+# a particular string starts, or is located at
+puts "programming".upcase() # makes all the char's in a string uppercase
