3260242843e3182118ad880181aaca02bc2a7078
Calherbe Valcin <cvalcin1988@gmail.com>
2019-12-29 15:42:33 -0500

    right at 28:34-Data Types Starts

---
+++ giraffe.rb
@@ -0,0 +1,8 @@
+character_name = "Cal"
+character_age = "35"
+
+puts ("There once was a man named #{character_name}")
+puts ("he was " + character_age + " years old.")
+character_name = ("Timmy")
+puts ("He really liked the name #{character_name}")
+puts ("but didnt like being " + character_age + ".")
